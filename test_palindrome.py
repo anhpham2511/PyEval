c7f51112fbcc0dd19cf28c063d87fe3b32284a4d
anhpham2511 <ms.phamanh@gmail.com>
2024-06-23 08:57:00 +0800

    the

---
+++ test_palindrome.py
@@ -0,0 +1,13 @@
+import pytest
+from test_palindrome_function import is_palindrome
+@pytest.mark.parametrize("maybe_palindrome, expected_result", [
+    ("", True),
+    ("a", True),
+    ("Bob", True),
+    ("Never odd or even", True),
+    ("Do geese see God?", True),
+    ("abc", False),
+    ("abab", False),
+])
+def test_is_palindrome(maybe_palindrome, expected_result):
+    assert is_palindrome(maybe_palindrome) == expected_result
